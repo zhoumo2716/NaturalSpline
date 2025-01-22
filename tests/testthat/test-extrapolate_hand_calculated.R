6679b0a47482533933fe4bb7993b077c49c9b06d
(Shirlina) Xuan Li <1208039950@qq.com>
2025-01-22 00:23:00 -0800

    Update test-extrapolate_hand_calculated.R

--- tests/testthat/test-extrapolate_hand_calculated.R
+++ tests/testthat/test-extrapolate_hand_calculated.R
@@ -1,6 +1,7 @@
 test_that("extrapolate_noncompact matches hand-calculated values", {
-  x2 <- c(1, 2, 2, 3, 4, 5)
-  theta2 <- c(2.5, 2.5, 2.5, 3, 4, 4)
+  x <- c(1, 2, 2, 3, 4, 5)
+  x_ <- c(1, 2, 3, 3, 4, 5)
+  theta <- c(2.5, 2.5, 2.5, 3, 4, 4)
   m1 <- 1
   m2 <- 2
   m3 <- 3
@@ -18,8 +19,8 @@ test_that("extrapolate_noncompact matches hand-calculated values", {
   result_right1_ <- extrapolate_noncompact(x, theta, m1, side = "right")
   expect_equal(result_right1_, expected_right1)
   
-  expected_left2 <- c(1, 2)
-  expected_right2 <- c(4, 5)
+  expected_left2 <- c(2, 2.5)
+  expected_right2 <- c(3.5, 4)
   
   result_left2 <- extrapolate_compact(x, theta, m2, side = "left")
   expect_equal(result_left2, expected_left2)
@@ -31,17 +32,17 @@ test_that("extrapolate_noncompact matches hand-calculated values", {
   result_right2_ <- extrapolate_noncompact(x, theta, m2, side = "right")
   expect_equal(result_right2_, expected_right2)
   
-  expected_left3 <- c(-2, 1, 2.125)
+  expected_left3 <- c(-2, 1, 3)
   expected_right3 <- c(2.5,2.5,2.5)
   
-  result_left3 <- extrapolate_compact(x, theta, m3, side = "left")
+  result_left3 <- extrapolate_compact(x_, theta, m3, side = "left")
   expect_equal(result_left3, expected_left3)
-  result_right3 <- extrapolate_compact(x, theta, m3, side = "right")
+  result_right3 <- extrapolate_compact(x_, theta, m3, side = "right")
   expect_equal(result_right3, expected_right3)
   
-  result_left3_ <- extrapolate_noncompact(x, theta, m3, side = "left")
+  result_left3_ <- extrapolate_noncompact(x_, theta, m3, side = "left")
   expect_equal(result_left3_, expected_left3)
-  result_right3_ <- extrapolate_noncompact(x, theta, m3, side = "right")
+  result_right3_ <- extrapolate_noncompact(x_, theta, m3, side = "right")
   expect_equal(result_right3_, expected_right3)
   
   
